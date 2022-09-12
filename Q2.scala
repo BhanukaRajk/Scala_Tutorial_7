456a5e31799d3a3b1764625269dbf7d45e60e57b
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2022-09-12 22:16:32 +0530

    Question 2 completed 2.0

--- Q2.scala
+++ Q2.scala
@@ -3,7 +3,7 @@ class Rational(x: Int, y: Int) {
     def Denominator = y;
     require(Denominator != 0, "Denominator must be a positive number");
 
-    // negation method
+    // subtraction method
     def sub(temp: Rational) = {
         if(this.Denominator == temp.Denominator)
             // If the denominators are equal
@@ -12,6 +12,7 @@ class Rational(x: Int, y: Int) {
             // If the denominators are not equal
             Rational((this.Numerator * temp.Denominator) - (temp.Numerator * this.Denominator), (this.Denominator * temp.Denominator));
     }
+
 
     def total() = {
         println(this.Numerator+"/"+this.Denominator);
@@ -22,16 +23,16 @@ class Rational(x: Int, y: Int) {
 
 object Q2{
     def main(args: Array[String]):Unit = {
-        // Rational numbers
+        // Given rational numbers
         val x = new Rational(3, 4);
         val y = new Rational(5, 8);
         val z = new Rational(2, 7);
 
-        val temp1 = x sub y;
-        val temp2 = temp1 sub z;
+        val temp1 = x sub y;        // Substract y from x
+        val temp2 = temp1 sub z;    // Substract z from previous calculated value
 
 
-        // Print rational numbers (negation)
+        // Print x-y-z value
         temp2.total();
     }
     
